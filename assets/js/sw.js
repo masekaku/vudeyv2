bb0076d68866c24b0b0c0c573317a66100d2871b
masekaku <aguslistiyanto27@gmail.com>
2025-10-15 20:56:41 +0700

    Delete assets directory

--- assets/js/sw.js
+++
@@ -1,9 +0,0 @@
-// Dummy Service Worker
-self.addEventListener('install', event => {
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-    // Dummy fetch handler
-    event.respondWith(fetch(event.request));
-});
