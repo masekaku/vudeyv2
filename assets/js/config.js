1149e878475c11a872babb2e1a66f17ba89532b7
masekaku <aguslistiyanto27@gmail.com>
2025-10-16 06:19:01 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,7 @@ window.APP_CONFIG = {
   PAGE_SIZE: 5,
   ARTIFACTS_JSON: '/artifacts.json',
   GA_MEASUREMENT_ID: 'G-XXXXXXXXXX', // replace with actual ID
-  HISTATS_ID: '00000', // replace with Histats ID
+  HISTATS_ID: '1,4984384,4,5,172,25,00011111', // replace with Histats ID
   DISQUS_SHORTNAME: 'your-disqus-shortname', // replace with actual shortname
   FIELD_MAP: { id: 'i', title: 't', thumbnail: 'm', link: 'l' },
   CACHE_VERSION: 'artifact-v1',
